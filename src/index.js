21f3955d34cfd12107718a3b416be00b3ed91582
Viktar Svintsitski <jetalai3@gmail.com>
2020-09-12 22:30:44 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix || matrix.length === 0) return [];
+  matrix = matrix.map(current => matrix.indexOf(current) % 2 === 0 ? current : current.reverse())
+  let result = recursiveSort(matrix);
+  return result;
+}
+
+function recursiveSort(matrix) {
+  if (matrix.filter(current => Array.isArray(current)).length > 0) {
+    matrix = matrix.flat();
+    return recursiveSort(matrix);
+  } else if (matrix.filter(current => Array.isArray(current)).length === 0) return matrix;
 }
